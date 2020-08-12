3d4f8bc802e047a2a0f1aae74a75ac8663a7cd4d
joe <joe.maliksi@gmail.com>
2020-08-11 21:04:01 -0400

    fix pypi docs

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 import setuptools
 
+
+with open('README.md', 'r') as f:
+    long_desc = f.read()
+
+
 setuptools.setup(
     name='blaseball-reference',
-    version='0.0.1',
+    version='0.0.2',
     author='Joe Maliksi',
     author_email='joe.maliksi@gmail.com',
     url='https://github.com/jmaliksi/blaseball-reference-py',
     description='Python wrapper around blaseball-reference API',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
 )
