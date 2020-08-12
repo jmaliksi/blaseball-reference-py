d139378ee98d98c315e3b3e838aac7f5d52542f4
joe <joe.maliksi@gmail.com>
2020-08-12 14:36:32 -0400

    are you sure

--- blaseball_reference/api.py
+++ blaseball_reference/api.py
@@ -22,11 +22,16 @@ def prepare_id(id_):
         raise ValueError(f'Incorrect ID type: {type(id_)}')
 
 
-def raw_events():
+def raw_events(**kwargs):
     """
     Download all of the game events, base runners, and player events. Child data (base runners, player events) are
 provided as their own lists and are not mapped into their parents, and thus must be matched by game_event_id.
+
+    This can cause a pretty hefty toll on the datablase, so use with caution. If you're absolutely sure, to prove
+    that you've read this, set the keyword argument `are_you_sure` to True.
     """
+    if not kwargs.get('are_you_sure'):
+        raise Exception('Please mind the datablase.')
     response = requests.get(construct_url('data/events'))
     response.raise_for_status()
     # I'm not going to try to format a raw data dump. This is on you.
