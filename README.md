eb0878b78ec22711a5f97d77baf3ebdee7d5662d
joe <joe.maliksi@gmail.com>
2020-08-11 20:57:51 -0400

    setup.py

--- README.md
+++ README.md
@@ -3,9 +3,15 @@ Python wrapper around the Blaseball stats API
 
 # Installation
 ```
+pip install blaseball-reference
+```
+
+# Usage
+https://api.blaseball-reference.com/docs#/
+
+# Development
+```
 python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
 ```
-
-TODO: proper pypi packaging
---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='blaseball-reference',
+    version='0.0.1',
+    author='Joe Maliksi',
+    author_email='joe.maliksi@gmail.com',
+    url='https://github.com/jmaliksi/blaseball-reference-py',
+    description='Python wrapper around blaseball-reference API',
+    packages=setuptools.find_packages(),
+)
